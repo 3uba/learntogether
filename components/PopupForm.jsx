705d9961859105385178e8c527c6ceff89ce1434
3uba <75246355+3uba@users.noreply.github.com>
2022-05-22 09:58:20 +0200

    sr

--- components/PopupForm.jsx
+++ components/PopupForm.jsx
@@ -28,35 +28,48 @@ const Popupform = ({ photoURL, name, setPopupForm }) => {
             onClick={() => closeForm()}
         >
             <form
-                className="w-[30vw] h-[75vh] text-[#fff] bg-[#3a3b3c] rounded-xl border-2 border-[#2a2b2c] p-3 relative"
+                className="w-[30vw] h-[75vh] text-[#fff] bg-[#3a3b3c] rounded-sm pt-3 px-8 relative"
                 onClick={(e) => e.stopPropagation()}
                 onSubmit={(e) => {
                     addPost(e, title, desc, file, closeForm);
                 }}
             >
-                <h1 className="w-[100%] text-4xl p-2 text-center broder-b-2 font-arvo">
+                <h1 className="w-[100%] text-4xl p-2 pb-4 text-center broder-b-2 font-arvo">
                     Create post
                 </h1>
                 <div className="flex flex-col items-center">
-                    <div className="flex items-center m-5 px-5 w-[100%]">
+                    <div className="flex items-center m-2 w-[100%]">
                         <img
                             src={photoURL}
                             alt=""
-                            className="rounded-full w-[50px]"
+                            className="rounded-full w-[2rem]"
                         />
-                        <p className="text-xl p-2">{name}</p>
+                        <p className="text-xl px-4">{name}</p>
                     </div>
-                    <div className="w-[100%] flex flex-col items-center m-3 py-2 ">
-                        <textarea
-                            type="text"
+                    <div className="w-[100%] flex flex-col items-center py-4">
+                        <select
+                            name=""
+                            id=""
                             onChange={(e) => setTitle(e.target.value)}
-                            className="form-control resize-none w-[90%] p-2 h-[5vh] bg-[#3a3b3c] text-xl text-[#fff] focus:border-none focus:outline-none"
-                            placeholder={`Add title of your problem `}
-                        />
+                            className="form-control px-[4px] py-2 resize-none w-[100%] h-[5vh] bg-[#3a3b3c] text-xl text-[#fff] focus:border-none focus:outline-none"
+                        >
+                            <option value="" selected disabled hidden>
+                                Choose your category
+                            </option>
+                            <option value="Math">Math</option>
+                            <option value="History">History</option>
+                            <option value="Biology">Biology</option>
+                            <option value="Spanish">Spanish</option>
+                            <option value="Geography">Geography</option>
+                            <option value="Chemistry">Chemistry</option>
+                            <option value="Physics">Physics</option>
+                            <option value="IT">IT</option>
+                            <option value="Other">Other</option>
+                        </select>
                         <textarea
                             type="text"
                             onChange={(e) => setDesc(e.target.value)}
-                            className="form-control resize-none w-[90%] p-2 h-[10vh] bg-[#3a3b3c] text-xl text-[#fff] focus:border-none focus:outline-none"
+                            className="form-control px-2 py-2 resize-none w-[100%] h-[20vh] bg-[#3a3b3c] text-xl text-[#fff] focus:border-none focus:outline-none"
                             placeholder={`Add description to specify the task`}
                         />
                     </div>
@@ -74,7 +87,7 @@ const Popupform = ({ photoURL, name, setPopupForm }) => {
                                 className="max-h-[20vh]"
                             />
                         ) : (
-                            <div className="w-[25vw] h-[20vh] border-2 flex flex-col items-center justify-center border-dotted">
+                            <div className="min-w-[28vw] w-[100%] h-[20vh] border-2 flex flex-col items-center justify-center border-dotted">
                                 <h1>Drop or get file</h1>
                                 <h5>[png, jpeg]</h5>
                             </div>
@@ -84,9 +97,9 @@ const Popupform = ({ photoURL, name, setPopupForm }) => {
 
                 <button
                     type="submit"
-                    className="bg-[#2a2b2c] hover:bg-[#242526] text-white font-bold py-2 px-4 rounded bottom-3 right-3 absolute"
+                    className="bg-[#2a2b2c] hover:bg-[#242526] text-white font-bold py-2 px-8 rounded-sm bottom-3 right-3 absolute"
                 >
-                    Publish
+                    Share
                 </button>
             </form>
         </div>
